c3c45c6cefb06bb462ee85fb94a0189c52646e4a
Lucas52352 <lucasrius86@gmail.com>
2024-08-26 12:38:13 -0300

    metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from "@/app/ui/invoices/edit-form";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Update Invoice',
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id
@@ -10,7 +15,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchCustomers(),
   ])
 
-  if(!invoice) { notFound() }
+  if (!invoice) { notFound() }
 
   return (
     <main>
